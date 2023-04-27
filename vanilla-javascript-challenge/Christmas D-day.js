e8280f258713e4522df5ddfe1f58f177cad1aa80
Hchan2 <seongchan_@naver.com>
2023-04-27 17:47:01 +0900

    크리스마스 디데이

---
+++ vanilla-javascript-challenge/Christmas D-day.js
@@ -0,0 +1,46 @@
+/**
+ * Challenge goals: Date와setInterval함수들을 사용해 크리스마스까지 며칠이 남았는지 알려주는 시계를 만드세요. (12월 25일)
+ */
+
+// 내가 작성한 코드
+const clockTitle = document.querySelector(".js-clock");
+const christmasDay = new Date("December 25, 2023 00:00:00").getTime();
+
+setInterval(() => {
+  const todayNow = new Date().getTime();
+  const remindDate = christmasDay - todayNow;
+
+  const days = Math.floor(remindDate / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (remindDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((remindDate % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remindDate % (1000 * 60)) / 1000);
+
+  clockTitle.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}, 1000);
+
+// NomadCoder Solution Code
+const clockTitle2 = document.querySelector(".js-clock");
+
+function getTime() {
+  const xmasDay = new Date(`${new Date().getFullYear()}-12-25:00:00:00+0900`);
+  const now = new Date();
+  // This is in milisecondsx
+  const difference = new Date(xmasDay - now);
+  const secondsInMs = Math.floor(difference / 1000);
+  const minutesInMs = Math.floor(secondsInMs / 60);
+  const hoursInMs = Math.floor(minutesInMs / 60);
+  const days = Math.floor(hoursInMs / 24);
+  const seconds = secondsInMs % 60;
+  const minutes = minutesInMs % 60;
+  const hours = hoursInMs % 24;
+  const daysStr = `${days < 10 ? `0${days}` : days}d`;
+  const hoursStr = `${hours < 10 ? `0${hours}` : hours}h`;
+  const minutesStr = `${minutes < 10 ? `0${minutes}` : minutes}m `;
+  const secondsStr = `${seconds < 10 ? `0${seconds}` : seconds}s`;
+  clockTitle2.innerHTML = `${daysStr} ${hoursStr} ${minutesStr} ${secondsStr}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
